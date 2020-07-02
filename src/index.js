01c67f941c60244be44676c832dd3dbe6acf7e6b
mihalyszijjartonagy <mihaly.szijjartonagy@prezi.com>
2020-07-02 11:37:57 +0200

    calculate winner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import {calculateWinner} from './helpers'
 
 function Square(props) {
     return (
@@ -28,13 +29,20 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) return;
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         const xIsNext = !this.state.xIsNext;
         this.setState({squares, xIsNext});
     }
 
     render() {
-        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'} `;
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'} `;
+        }
 
         return (
             <div>
